7ef32a76f6e9c0a81a0707ee846a711af4eb9396
CodeWithVaishu <vaishnavipangaonkars22@gmail.com>
2026-05-12 21:53:18 +0530

    Defined resource group

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,1 +1,4 @@
-
+resource "azurerm_resource_group" "rg" {
+  name     = "my-terraform-rg"
+  location = "Central India"
+}
--- terraform/var.tf
+++ terraform/var.tf
@@ -1,1 +1,10 @@
+variable "resource_group_name" {
+  description = "my-terraform-rg"
+  type        = string
+}
 
+variable "location" {
+  description = "Azure Region"
+  type        = string
+  default     = "Central India"
+}
