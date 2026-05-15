819bc4ba4cda43a8d8af42180a297ed099a35ada
CodeWithVaishu <vaishnavipangaonkars22@gmail.com>
2026-05-15 23:01:33 +0530

    Defined new RG

--- terraform/main.tf
+++ terraform/main.tf
@@ -2,3 +2,8 @@ resource "azurerm_resource_group" "my-resource-group" {
   name     = var.resource_group_name
   location = var.location
 }
+
+resource "azurerm_resource_group" "vaishnavi-rg-2"{
+  name     = var.resource_group_name
+  location = var.location
+}
