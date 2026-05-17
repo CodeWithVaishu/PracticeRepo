7a85ad1141e283a1775ebd260ea25f6962970edb
CodeWithVaishu <vaishnavipangaonkars22@gmail.com>
2026-05-17 22:27:09 +0530

    Defined subnet

--- terraform/vm_deployment.tf
+++ terraform/vm_deployment.tf
@@ -14,9 +14,9 @@ resource "azurerm_virtual_network" "vnet1" {
 }
 
 resource "azurerm_subnet" "subnet1" {
-  name                = "subnet1"
-  address_space       = ["10.0.0.0/24"]
-  virtual_network     = azurerm_virtual_network.vnet1.name
-  resource_group_name = azurerm_resource_group.vaishnavi_rg_3.name
+  name                    = "subnet1"
+  address_prefixes        = ["10.0.0.0/24"]
+  virtual_network_name    = azurerm_virtual_network.vnet1.name
+  resource_group_name     = azurerm_resource_group.vaishnavi_rg_3.name
 }
 
