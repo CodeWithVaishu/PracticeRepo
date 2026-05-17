d93d290f8589499270e5f76c757019b2993cf59b
CodeWithVaishu <vaishnavipangaonkars22@gmail.com>
2026-05-17 21:38:17 +0530

    defined vnet

--- terraform/main.tf
+++ terraform/main.tf
@@ -3,11 +3,8 @@ resource "azurerm_resource_group" "my-resource-group" {
   location = var.location
 }
 
-resource "azurerm_resource_group" "vaishnavi-rg-2" {
+resource "azurerm_resource_group" "vaishnavi_rg_2" {
   name     = var.resource_group_name2
   location = var.location
 }
- resource "azurerm_virtual_machine" "vaishnavi-vm" {
-   name     = var.vm_name
-   location = var.location
- }
+
--- terraform/var.tf
+++ terraform/var.tf
@@ -13,13 +13,13 @@ variable "location" {
 variable "resource_group_name2" {
   description = "my-resource-group2"
   type        = string
-  default     = "vaishnavi-rg-2"
+  default     = "vaishnavi_rg_2"
 }
 
 variable "resource_group_name3" {
   description = "resource-group3"
   type        = string
-  default     = "vaishnavi-rg-3"
+  default     = "vaishnavi_rg_3"
 }
 
 variable "virtual_network1" {
