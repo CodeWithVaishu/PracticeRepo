9b874faa5166918000dddcb5c5f23cc8657d86a1
CodeWithVaishu <vaishnavipangaonkars22@gmail.com>
2026-05-17 21:58:10 +0530

    defined vnet

--- terraform/vm_deployment.tf
+++ terraform/vm_deployment.tf
@@ -10,7 +10,7 @@ resource "azurerm_virtual_network" "vnet1" {
   name               = var.virtual_network1
   address_space      = ["10.0.0.0/16"]
   location           = var.location 
-  resouce_group_name = azurerm_resouce_group.vaishnavi_rg_3.name
+  resouce_group_name = azurerm_resource_group.vaishnavi_rg_3.name
 }
 
 
